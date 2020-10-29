bb56239a648af798bc740ba65fe67b23a1bcb437
wecassidy <wec.notif.github@gmail.com>
2020-10-29 18:23:39 -0400

    Open file dialogue

--- app.py
+++ app.py
@@ -4,16 +4,29 @@
 Application to label ENPH 353 license plate videos
 """
 
+import os
 import sys
 
 from PyQt5 import QtWidgets
 from python_qt_binding import loadUi
+
+VIDEO_LOCATION = os.path.expanduser("~/Videos/353_recordings")
 
 
 class LabelApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("labeller.ui", self)
+
+        self.file = None
+        self.action_open.triggered.connect(self.open_video)
+
+    def open_video(self):
+        self.file = QtWidgets.QFileDialog.getOpenFileName(
+            None, "Open Video", VIDEO_LOCATION, "Video Files (*.avi)"
+        )[0]
+
+        print(f"Opening {self.file}")
 
 
 if __name__ == "__main__":
